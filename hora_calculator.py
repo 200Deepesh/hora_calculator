7c46d8f0d173e5578987a8ac933fa56706099e36
Deepesh Gupta <learning.is.first.earning@2006>
2025-05-24 11:52:51 +0530

    quick update

--- hora_calculator.py
+++ hora_calculator.py
@@ -22,10 +22,14 @@ planetNamesIndex = {
 def validate_inputdata(date_text, city):
     try:
         date.fromisoformat(date_text) #show error if the date is not in correct format
-        loc = geopy.Nominatim(user_agent='hora calculater').geocode(city) #show error for invalid city name
-        return True          
+        try:
+            loc = geopy.Nominatim(user_agent='hora calculater').geocode(city) #show error for invalid city name
+            return True 
+        except: 
+            messagebox.showerror("Invalid Date", "Please enter the valid city name or check your internet connection.")
+            return False        
     except:
-        messagebox.showerror("Invalid Date", "Please enter the valid city name or date in YYYY/MM/DD format.")
+        messagebox.showerror("Invalid Date", "Please enter the valid city name or date in YYYY-MM-DD format.")
         return False
 
 def submit_date():
@@ -69,7 +73,7 @@ def submit_date():
         tt = firstSR
         for i in range(0,2):
             for j in range(0,12):
-                hc.append([planetNames[index],tt.time().isoformat(timespec='seconds'),(tt+td[i]).time().isoformat(timespec='seconds')])
+                hc.append([j+1,planetNames[index],tt.time().isoformat(timespec='seconds'),(tt+td[i]).time().isoformat(timespec='seconds')])
                 tt = td[i] + tt
                 index += 1
                 if(index == 7):
@@ -93,14 +97,14 @@ def clear():
 
 #GUI of app
 root = tk.Tk()
-root.geometry('300x600')
+root.geometry('300x800')
 root.title("Date Input")
 
 #taking date as input
 tk.Label(root, text="Enter your city name:").pack(pady=10)
 city_name = tk.Entry(root)
 city_name.pack(pady=5)
-tk.Label(root, text="Enter Date (YYYY/MM/DD):").pack(pady=10)
+tk.Label(root, text="Enter Date (YYYY-MM-DD):").pack(pady=10)
 date_entry = tk.Entry(root)
 date_entry.pack(pady=5)
 
@@ -110,13 +114,15 @@ tk.Button(root, text="Submit", command=submit_date).pack(pady=10)
 horaChart = ttk.Treeview(root, height=24)
 horaChart.pack()
 
-horaChart['columns']= ['planet name', 'from', 'to']
+horaChart['columns']= ['S.NO','planet name', 'from', 'to']
 horaChart.column("#0", width=0,  stretch=tk.NO)
+horaChart.column("S.NO",anchor=tk.CENTER, width=40)
 horaChart.column("planet name",anchor=tk.CENTER, width=80)
 horaChart.column("from",anchor=tk.CENTER, width=80)
 horaChart.column("to",anchor=tk.CENTER, width=80)
 
 horaChart.heading("#0",text="",anchor=tk.CENTER)
+horaChart.heading("S.NO",text="S.NO",anchor=tk.CENTER)
 horaChart.heading("planet name",text="Planet Name",anchor=tk.CENTER)
 horaChart.heading("from",text="From",anchor=tk.CENTER)
 horaChart.heading("to",text="To",anchor=tk.CENTER)
